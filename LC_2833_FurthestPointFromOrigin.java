3353dbf341b2d898f217b774ccc60436559c6efa
Aditi Jar <aditijar13@gmail.com>
2026-04-26 22:51:20 +0530

    Implement method to calculate furthest distance from origin

--- LC_2833_FurthestPointFromOrigin.java
+++ LC_2833_FurthestPointFromOrigin.java
@@ -1,1 +1,14 @@
 
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int left = 0, right = 0, wildcard = 0;
+
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'L') left++;
+            else if (ch == 'R') right++;
+            else wildcard++;
+        }
+
+        return Math.abs(left - right) + wildcard;
+    }
+}
