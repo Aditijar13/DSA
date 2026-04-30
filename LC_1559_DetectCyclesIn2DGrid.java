ec03bb371c675de418204da2c0bc8e2b86fa602d
Aditi Jar <aditijar13@gmail.com>
2026-05-01 05:16:41 +0530

    Implement cycle detection in 2D grid

--- LC_1559_DetectCyclesIn2DGrid.java
+++ LC_1559_DetectCyclesIn2DGrid.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public boolean containsCycle(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        boolean[][] visited = new boolean[n][m];
+        
+        int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j]) {
+                    if (bfs(grid, visited, i, j, directions)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
