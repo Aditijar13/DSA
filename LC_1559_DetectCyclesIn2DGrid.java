fc5f69c04a20ea29ef9fddaa40672ec7a1297b46
Aditi Jar <aditijar13@gmail.com>
2026-05-01 05:17:12 +0530

    Update LC_1559_DetectCyclesIn2DGrid.java

--- LC_1559_DetectCyclesIn2DGrid.java
+++ LC_1559_DetectCyclesIn2DGrid.java
@@ -19,3 +19,38 @@ class Solution {
         
         return false;
     }
+
+     private boolean bfs(char[][] grid, boolean[][] visited, int startX, int startY, int[][] directions) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{startX, startY, -1, -1});
+        visited[startX][startY] = true;
+        
+        char ch = grid[startX][startY];
+        
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int x = curr[0], y = curr[1];
+            int parentX = curr[2], parentY = curr[3];
+            
+            for (int[] dir : directions) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+                
+                if (nx >= 0 && ny >= 0 && nx < grid.length && ny < grid[0].length
+                    && grid[nx][ny] == ch) {
+                    
+                    if (!visited[nx][ny]) {
+                        queue.offer(new int[]{nx, ny, x, y});
+                        visited[nx][ny] = true;
+                    } 
+                    else if (nx != parentX || ny != parentY) {
+                        // 🔥 Cycle detected
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+}
