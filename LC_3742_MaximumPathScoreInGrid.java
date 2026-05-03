941f67d433566a95ece4832e9b237cd77c99a7b7
Aditi Jar <aditijar13@gmail.com>
2026-05-04 01:56:06 +0530

    Update LC_3742_MaximumPathScoreInGrid.java

--- LC_3742_MaximumPathScoreInGrid.java
+++ LC_3742_MaximumPathScoreInGrid.java
@@ -1,1 +1,16 @@
+class Solution {
+    private int[][] grid;
+    private Integer[][][] dp;
+    private final int NEG_INF = (int)-1e9;
 
+    public int maxPathScore(int[][] grid, int k) {
+        this.grid = grid;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        dp = new Integer[m][n][k + 1];
+
+        int ans = dfs(m - 1, n - 1, k);
+
+        return ans < 0 ? -1 : ans;
+    }
