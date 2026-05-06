31e27961f9539d7c7ac4e3bfb7d65e71984da1d6
Aditi Jar <aditijar13@gmail.com>
2026-05-06 16:04:06 +0530

    Update LC_796_RotateString.java

--- LC_796_RotateString.java
+++ LC_796_RotateString.java
@@ -1,5 +1,17 @@
 // Problem: Rotate String
 // Platform: LeetCode
+// Approach: String Concatenation
+//
+// Idea:
+// If goal is a rotation of s,
+// then it must appear inside (s + s)
+//
+// Example:
+// s = "abcde"
+// s+s = "abcdeabcde"
+// goal = "cdeab" exists inside it
+//
+
 class Solution {
     public boolean rotateString(String s, String goal) {
         return s.length() == goal.length() &&
