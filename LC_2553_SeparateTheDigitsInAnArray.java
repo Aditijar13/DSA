d6b1a6c4940d48f3a81254473d81bc02cc812de5
Aditi Jar <aditijar13@gmail.com>
2026-05-11 20:45:47 +0530

    Update LC_2553_SeparateTheDigitsInAnArray.java

--- LC_2553_SeparateTheDigitsInAnArray.java
+++ LC_2553_SeparateTheDigitsInAnArray.java
@@ -1,1 +1,26 @@
 
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        List<Integer> resultList = new ArrayList<>();
+
+        for (int number : nums) {
+            List<Integer> digits = new ArrayList<>();
+
+            while (number > 0) {
+                digits.add(number % 10);
+                number /= 10;
+            }
+
+            Collections.reverse(digits);
+            resultList.addAll(digits);
+        }
+
+        int[] result = new int[resultList.size()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = resultList.get(i);
+        }
+
+        return result;
+    }
+}
