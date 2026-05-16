7c7838656150a1d75eb2ed6da0737048c80746be
Aditi Jar <aditijar13@gmail.com>
2026-05-16 22:05:17 +0530

    Update LC_153_FindMinimumInRotatedSortedArray.java

--- LC_153_FindMinimumInRotatedSortedArray.java
+++ LC_153_FindMinimumInRotatedSortedArray.java
@@ -1,1 +1,36 @@
 
+
+public class Solution {
+
+    public int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        int minimum = nums[0];
+
+        while (left <= right) {
+
+            // If current portion is already sorted
+            if (nums[left] < nums[right]) {
+                minimum = Math.min(minimum, nums[left]);
+                break;
+            }
+
+            int mid = left + (right - left) / 2;
+
+            minimum = Math.min(minimum, nums[mid]);
+
+            // Left half is sorted
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1;
+            }
+            // Right half is sorted
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return minimum;
+    }
+}
