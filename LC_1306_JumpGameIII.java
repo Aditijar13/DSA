f0546c3a6e684c6c37d7997fb5be244365ee6459
Aditi Jar <aditijar13@gmail.com>
2026-05-17 16:07:39 +0530

    Update LC_1306_JumpGameIII.java

--- LC_1306_JumpGameIII.java
+++ LC_1306_JumpGameIII.java
@@ -1,1 +1,49 @@
 
+import java.util.*;
+
+class Solution {
+
+    public boolean canReach(int[] arr, int start) {
+
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+
+            int currentIndex = queue.poll();
+
+            // Reached value 0
+            if (arr[currentIndex] == 0) {
+                return true;
+            }
+
+            int jumpDistance = arr[currentIndex];
+
+            // Mark as visited
+            arr[currentIndex] = -1;
+
+            // Possible next positions
+            int forward = currentIndex + jumpDistance;
+            int backward = currentIndex - jumpDistance;
+
+            // Forward jump
+            if (forward >= 0 &&
+                forward < arr.length &&
+                arr[forward] >= 0) {
+
+                queue.offer(forward);
+            }
+
+            // Backward jump
+            if (backward >= 0 &&
+                backward < arr.length &&
+                arr[backward] >= 0) {
+
+                queue.offer(backward);
+            }
+        }
+
+        return false;
+    }
+}
