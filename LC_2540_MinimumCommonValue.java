490d336c0d4f65faeace4309133ec37a59e668e9
Aditi Jar <aditijar13@gmail.com>
2026-05-19 12:40:59 +0530

    Update LC_2540_MinimumCommonValue.java

--- LC_2540_MinimumCommonValue.java
+++ LC_2540_MinimumCommonValue.java
@@ -1,4 +1,24 @@
-
+/**
+ * Problem: 2540. Minimum Common Value
+ * Platform: LeetCode
+ * 
+ * Approach:
+ * - Since both arrays are already sorted in non-decreasing order,
+ *   use the two-pointer technique.
+ * - Start one pointer at the beginning of each array.
+ * - If both values are equal, return that value because it is
+ *   the smallest common element encountered.
+ * - If nums1[pointer1] < nums2[pointer2], move pointer1.
+ * - Otherwise move pointer2.
+ * 
+ * Idea:
+ * - Similar to merging two sorted arrays.
+ * - We eliminate smaller values because they cannot become
+ *   the minimum common value later.
+ * 
+ * Time Complexity: O(n + m)
+ * Space Complexity: O(1)
+ */
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         int pointer1 = 0;
