8bef70671d25582b5f394f77a6dfbd732685c713
Aditi Jar <aditijar13@gmail.com>
2026-05-23 19:28:30 +0530

    Update LC_1752_CheckIfArrayIsSortedAndRotated.java

--- LC_1752_CheckIfArrayIsSortedAndRotated.java
+++ LC_1752_CheckIfArrayIsSortedAndRotated.java
@@ -1,1 +1,24 @@
 
+class Solution {
+
+    public boolean check(int[] nums) {
+
+        int breakCount = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int next = (i + 1) % n;
+
+            if (nums[i] > nums[next]) {
+                breakCount++;
+            }
+
+            if (breakCount > 1) {
+                return false; // early exit optimization
+            }
+        }
+
+        return breakCount <= 1;
+    }
+}
