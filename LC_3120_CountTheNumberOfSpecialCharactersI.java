bc10d188e03b2fef31cb13c43ee4f31a835f55af
Aditi Jar <aditijar13@gmail.com>
2026-05-26 20:13:48 +0530

    Update LC_3120_CountTheNumberOfSpecialCharactersI.java

--- LC_3120_CountTheNumberOfSpecialCharactersI.java
+++ LC_3120_CountTheNumberOfSpecialCharactersI.java
@@ -1,1 +1,18 @@
+/*
+ * Problem: Count the Number of Special Characters I
+ * Platform: LeetCode
+ * Problem Number: 3120
+ *
+ * Approach:
+ * - Use a boolean presence array to track
+ *   whether each character exists.
+ * - Traverse the string and mark characters.
+ * - Check for each alphabet whether both
+ *   lowercase and uppercase versions exist.
+ * - Count such characters.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ *      (Fixed size array)
+ */
 
