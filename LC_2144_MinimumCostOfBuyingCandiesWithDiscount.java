d4f6719d61e3f82931e300c8d5fba180c44e5069
Aditi Jar <aditijar13@gmail.com>
2026-06-01 23:42:21 +0530

    Update LC_2144_MinimumCostOfBuyingCandiesWithDiscount.java

--- LC_2144_MinimumCostOfBuyingCandiesWithDiscount.java
+++ LC_2144_MinimumCostOfBuyingCandiesWithDiscount.java
@@ -1,1 +1,28 @@
 
+
+import java.util.Arrays;
+
+class Solution {
+
+    public int minimumCost(int[] cost) {
+
+        Arrays.sort(cost);
+
+        int totalCost = 0;
+
+        for (int i = cost.length - 1; i >= 0; i -= 3) {
+
+            // Most expensive candy
+            totalCost += cost[i];
+
+            // Second most expensive candy
+            if (i > 0) {
+                totalCost += cost[i - 1];
+            }
+
+            // Third candy is free
+        }
+
+        return totalCost;
+    }
+}
