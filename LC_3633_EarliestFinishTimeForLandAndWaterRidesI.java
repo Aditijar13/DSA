07a35d8823fb04c931f107239ae14e9ae926922b
Aditi Jar <aditijar13@gmail.com>
2026-06-02 09:30:46 +0530

    Document problem and approach in LC_3633
    Added problem statement and approach for the solution.

--- LC_3633_EarliestFinishTimeForLandAndWaterRidesI.java
+++ LC_3633_EarliestFinishTimeForLandAndWaterRidesI.java
@@ -1,4 +1,29 @@
-
+/*
+ * Problem: Earliest Finish Time for Land and Water Rides I
+ * Platform: LeetCode
+ * Problem Number: 3633
+ *
+ * Approach:
+ * - Try both possible orders:
+ *      1. Land Ride -> Water Ride
+ *      2. Water Ride -> Land Ride
+ *
+ * - For a given order:
+ *      a) Find the earliest completion time among all rides
+ *         of the first type.
+ *      b) For each ride of the second type:
+ *           start = max(firstCompletionTime, secondStartTime)
+ *           finish = start + secondDuration
+ *      c) Take the minimum finish time.
+ *
+ * - Return the minimum answer from both orders.
+ *
+ * Time Complexity: O(n + m)
+ *      n = number of land rides
+ *      m = number of water rides
+ *
+ * Space Complexity: O(1)
+ */
 class Solution {
     public int earliestFinishTime(
         int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
