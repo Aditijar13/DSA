740d4c176fe8fa7596ee823f8443e390494fbef5
Aditi Jar <aditijar13@gmail.com>
2026-06-06 22:20:24 +0530

    Update LC_2574_LeftAndRightDifferenceArray.java

--- LC_2574_LeftAndRightDifferenceArray.java
+++ LC_2574_LeftAndRightDifferenceArray.java
@@ -17,22 +17,22 @@
  * Space Complexity: O(1) extra (excluding output array)
  */
 class Solution {
-    public int[] leftRigthDifference(int[] nums) {
+    public int[] leftRightDifference(int[] nums) {
+        int rightSum = 0;
+
+        for (int num : nums) {
+            rightSum += num;
+        }
+
         int leftSum = 0;
-        int rightSum = Arrays.stream(nums).sum();
-      
-        int n = nums.length;
-      
-        int[] result = new int[n];
-      
-        for (int i = 0; i < n; i++) {
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
             rightSum -= nums[i];
-          
             result[i] = Math.abs(leftSum - rightSum);
-          
             leftSum += nums[i];
         }
-      
+
         return result;
     }
 }
