a559093fdd7587159eb8679690842b7c8170a930
Aditi Jar <aditijar13@gmail.com>
2026-06-09 01:32:27 +0530

    Update LC_2161_PartitionArrayAccordingToGivenPivot.java

--- LC_2161_PartitionArrayAccordingToGivenPivot.java
+++ LC_2161_PartitionArrayAccordingToGivenPivot.java
@@ -18,3 +18,32 @@
  * Space Complexity: O(n)
  *      Result array.
  */
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+      
+        int[] result = new int[n];
+      
+        int index = 0;
+      
+        for (int num : nums) {
+            if (num < pivot) {
+                result[index++] = num;
+            }
+        }
+      
+        for (int num : nums) {
+            if (num == pivot) {
+                result[index++] = num;
+            }
+        }
+      
+        for (int num : nums) {
+            if (num > pivot) {
+                result[index++] = num;
+            }
+        }
+      
+        return result;
+    }
+}
