eb981528798fba9bdd6452075d701f350b00d452
Aditi Jar <aditijar13@gmail.com>
2026-06-14 12:51:33 +0530

    Update LC_2130_MaximumTwinSumOfALinkedList.java

--- LC_2130_MaximumTwinSumOfALinkedList.java
+++ LC_2130_MaximumTwinSumOfALinkedList.java
@@ -1,1 +1,14 @@
-
+/*
+ * Problem: Maximum Twin Sum of a Linked List
+ * Platform: LeetCode
+ * Problem Number: 2130
+ *
+ * Approach:
+ * - Store all node values in an ArrayList.
+ * - Use two-pointer indexing:
+ *      i and (n - 1 - i)
+ * - Compute twin sums and track maximum.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
