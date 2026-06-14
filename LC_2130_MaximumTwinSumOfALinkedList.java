908a00ccc8f6a7638d0f9316a70148398dd25af9
Aditi Jar <aditijar13@gmail.com>
2026-06-14 12:52:04 +0530

    Update LC_2130_MaximumTwinSumOfALinkedList.java

--- LC_2130_MaximumTwinSumOfALinkedList.java
+++ LC_2130_MaximumTwinSumOfALinkedList.java
@@ -12,3 +12,32 @@
  * Time Complexity: O(n)
  * Space Complexity: O(n)
  */
+class Solution {
+
+    public int pairSum(ListNode head) {
+
+        List<Integer> values = new ArrayList<>();
+
+        ListNode current = head;
+
+        while (current != null) {
+            values.add(current.val);
+            current = current.next;
+        }
+
+        int maxTwinSum = 0;
+        int n = values.size();
+
+        for (int i = 0; i < n / 2; i++) {
+
+            int twinSum =
+                    values.get(i) +
+                    values.get(n - 1 - i);
+
+            maxTwinSum =
+                    Math.max(maxTwinSum, twinSum);
+        }
+
+        return maxTwinSum;
+    }
+}
